34c8413703a509745f2e8d1b45068ed7669ae397
Eusebie Siebenberg <siebenee@gmail.com>
2023-06-09 15:58:31 -0700

    Bugfix: Update RestRequest Urls to versioning specifics on ApiHelper.cs

--- AnimalShelterClient/Models/ApiHelper.cs
+++ AnimalShelterClient/Models/ApiHelper.cs
@@ -8,21 +8,21 @@ namespace AnimalShelterClient.Models
     public static async Task<string> GetAll()
     {
       RestClient client = new RestClient("http://localhost:5000/");
-      RestRequest request = new RestRequest($"api/animals", Method.Get);
+      RestRequest request = new RestRequest($"api/v2/animals", Method.Get);
       RestResponse response = await client.GetAsync(request);
       return response.Content;
     }
     public static async Task<string> Get(int id)
     {
       RestClient client = new RestClient("http://localhost:5000/");
-      RestRequest request = new RestRequest($"api/animals/{id}", Method.Get);
+      RestRequest request = new RestRequest($"api/v2/animals/{id}", Method.Get);
       RestResponse response = await client.GetAsync(request);
       return response.Content;
     }
     public static async void Post(string newAnimal)
     {
       RestClient client = new RestClient("http://localhost:5000/");
-      RestRequest request = new RestRequest($"api/animals", Method.Post);
+      RestRequest request = new RestRequest($"api/v2/animals", Method.Post);
       request.AddHeader("Content-Type", "application/json");
       request.AddJsonBody(newAnimal);
       await client.PostAsync(request);
@@ -31,7 +31,7 @@ namespace AnimalShelterClient.Models
     public static async void Put(int id, string newAnimal)
     {
       RestClient client = new RestClient("http://localhost:5000/");
-      RestRequest request = new RestRequest($"api/animals/{id}", Method.Put);
+      RestRequest request = new RestRequest($"api/v2/animals/{id}", Method.Put);
       request.AddHeader("Content-Type", "application/json");
       request.AddJsonBody(newAnimal);
       await client.PutAsync(request);
